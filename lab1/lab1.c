8f52d6aecdd4bd23f6764aaa3c56dd59c96d4894
Matea Stanišić <stmatea@student.math.hr>
2019-04-03 00:47:32 +0200

    Rijesene Vjezbe1!

---
+++ lab1/lab1.c
@@ -0,0 +1,173 @@
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+
+
+#define NAJVECI_BROJ 0xffffffffffffffffULL
+#define A 8531648002905263869ULL
+#define B 18116652324302926351ULL
+
+
+typedef unsigned long long int ulli;
+
+
+bool first_time = true; 
+/*jer se static varijabla u c-u ne može inicijalizirati s nekom ne-konstantnom vrijednošću
+pa nam treba info generiramo li prvi puta neki random broj ( rnd = time(NULL) ) ili ne 
+( rnd = ( rnd*A )%B )*/
+ulli rnd; //static varijabla
+
+ulli MS[5]; //međuspremnik
+
+int I, U; //izlaz index, ulaz index inicijalizirani na 0
+
+double t; //za provjeru zahtjeva
+
+double start_time; //da znamo koliko je vremena prošlo
+
+
+//funkcija za sekunde
+double dsecnd (void) {
+    return (double)( clock( ) ) / CLOCKS_PER_SEC;
+}
+
+ulli random_64bit();
+
+bool test_bitovi( ulli broj );
+
+bool test_pseudo_prost( ulli broj );
+
+const char* provjera_zahtjeva();
+
+
+
+int main() {	
+	t = dsecnd();
+	start_time = dsecnd();
+
+	do {
+		ulli x = random_64bit() | 1ULL; //da bude neparan
+
+		int br = 0, br2 = 0; //brojači za zaustavljanje petlje i op. x+=2
+		int limit = 100; //koliko puta ćemo dodavati 2
+
+		while ( test_bitovi(x) == false || test_pseudo_prost(x) == false ) {
+			if( x <= (NAJVECI_BROJ - 2) && br < limit ) {
+				x = x + 2ULL;
+				br++;
+			}
+			else {
+				x = random_64bit() | 1ULL; //da bude neparan
+				br = 0;
+			}
+			br2++;
+			if( br2 >= 3000000000 ) {
+				break; //zaustavlja petlju - predugo tražimo
+			} 
+		}
+		if( br2 >= 3000000000 ) {
+			br = 0;
+			//opet ista stvar, samo ovdje ne gledamo uvjet test_bitovi(x)
+			while ( test_pseudo_prost(x) == false ) {
+				if ( x <= (NAJVECI_BROJ - 2) && br < limit ) {
+					x = x + 2ULL;
+					br++;
+				}
+				else {
+					x = random_64bit() | 1ULL; //da bude neparan
+					br = 0;
+				}
+			}
+		}
+
+		//dodaj x u MS
+		MS[U] = x; 
+		U = ( U + 1 ) % 5;
+
+	} while ( strcmp( "KRAJ_RADA", provjera_zahtjeva() ) != 0 );
+
+	return 0;
+}
+
+
+ulli random_64bit() {
+	if( first_time ) {
+		first_time = false;
+		rnd = time(NULL);
+	}
+	else {
+		rnd = ( rnd * A ) % B;
+	} 
+	return rnd;
+}
+
+
+bool test_bitovi( ulli broj ) {
+	for( int i = 0; i < 62; i++ ) { 
+		//do 62 jer gledamo 3 bita u nizu pa ne trebamo ići do 63-eg bita
+		bool i1 = ( (broj & ( 1ULL << i ) ) != 0 );
+		bool i2 = ( (broj & ( 1ULL << ( i + 1 ) ) ) != 0 );
+		bool i3 = ( (broj & ( 1ULL << ( i + 2 ) ) ) != 0 );
+
+		if( i1 == i2 && i1 == i3 ) {
+			return false; //tranzitivnost povlači i2 == i3
+		} 
+	}
+	return true;
+}
+
+
+bool test_pseudo_prost( ulli broj ) {
+	ulli n;
+
+	//ako je 'sqrt(broj)' prevelik, gledamo je li prost do 50 000
+	if( sqrt(broj) > 50000 ) {
+		n = 50000;
+	} 
+	else {
+		n = sqrt(broj);
+	}
+	
+	for( unsigned int i = 3; i <= n; i += 2 ) {
+		if( broj % i == 0 ) return false;
+	}
+	return true;
+}
+
+
+const char* provjera_zahtjeva() {
+	double t2 = t;
+
+	t = dsecnd();
+	if( ( t - t2 ) < 1 ) {
+		//nije prošla bar 1s od zadnje provjere zahtjeva
+		return "NIJE_KRAJ";
+	}
+	else {
+		int r = rand() % 100 + 1; //r u rasponu od 1 do 100
+		if( r >= 50 ){ //postoji novi zahtjev (vjerojatnost 50%)
+			//vrijeme proteklo od početka programa
+			int time_passed = dsecnd() - start_time;
+			printf("%03d - ", time_passed ); //ispisuje vrijeme
+
+			printf("MS[] = { ");
+			for( int i = 0; i < 5; i++ ) printf("%2llx ", (MS[i] & 255) );
+			printf("} ");
+
+			printf("I=%d", I);
+			printf(", U=%d", U);
+			printf(", ");
+
+			printf("MS[I]=%#016llx ", MS[I]);
+			I = ( I + 1 ) % 5;
+			printf("\n");
+		}
+
+		r = rand() % 100 + 1;
+		if( r <= 10 ) return "KRAJ_RADA"; //postoji zahtjev za kraj rada ( vjerojatnost 10% )
+		else return "NIJE_KRAJ";
+	}
+}
---
+++ lab1/readme.txt
@@ -0,0 +1,50 @@
+$ gcc lab1.c -lm -Wall 
+$ ./a.out
+002 - MS[] = { a5 9b  0  0  0 } I=0, U=2, MS[I]=0x492d2cc92b3252a5 
+003 - MS[] = { a5 9b b5  0  0 } I=1, U=3, MS[I]=0x6a524d326d52b49b 
+005 - MS[] = { a5 9b b5 ad  0 } I=2, U=4, MS[I]=0x64da9696b52cacb5 
+006 - MS[] = { 53 9b b5 ad 35 } I=3, U=1, MS[I]=0x4daca6a9ab6949ad 
+008 - MS[] = { 53 99 b5 ad 35 } I=4, U=2, MS[I]=0x9ab3665a94aa9335 
+010 - MS[] = { 4b 99 35 2b 5b } I=0, U=1, MS[I]=0xccd99296d2ca4b4b 
+012 - MS[] = { 4b 6d 59 2b 5b } I=1, U=3, MS[I]=0xb64d5ab59935336d 
+015 - MS[] = { 4b 6d 59 93 5b } I=2, U=4, MS[I]=0x6ca99926649b2d59 
+019 - MS[] = { 93 69 a5 33 d5 } I=3, U=0, MS[I]=0xd354a52925265933  
+$ ./a.out
+003 - MS[] = { 49  0  0  0  0 } I=0, U=1, MS[I]=0x6caaca6aa949b549 
+006 - MS[] = { 49 55  0  0  0 } I=1, U=2, MS[I]=0x32b6a5b5a5a95a55 
+007 - MS[] = { 49 55 25  0  0 } I=2, U=3, MS[I]=0x569356d5b5255525 
+012 - MS[] = { 59 93 95 6b b3 } I=3, U=4, MS[I]=0x9ad9695b6b335a6b 
+016 - MS[] = { 25 25 95 6b 99 } I=4, U=2, MS[I]=0xcb69969b6655a499 
+017 - MS[] = { 25 25 d5 6b 99 } I=0, U=3, MS[I]=0x652cb4cca4956d25 
+021 - MS[] = { 5b 4d d5 a5 cd } I=1, U=2, MS[I]=0xab6959b2aad95a4d 
+022 - MS[] = { 5b 4d 55 4d 4d } I=2, U=0, MS[I]=0x4cad32a94aaab655 
+028 - MS[] = { b5 b5 59 d9 95 } I=3, U=2, MS[I]=0xd5364949a92a6cd9 
+031 - MS[] = { b5 b5 cb a9 95 } I=4, U=4, MS[I]=0xda92a56a4b555495 
+033 - MS[] = { db b5 cb a9 a5 } I=0, U=1, MS[I]=0x5249b52c9a6a52db 
+035 - MS[] = { db 59 ad 53 a5 } I=1, U=4, MS[I]=0x66dada5ad66ad259 
+038 - MS[] = { 95 99 35 99 4b } I=2, U=4, MS[I]=0x6994d52cd2ab6935  
+$ ./a.out
+001 - MS[] = { c9  0  0  0  0 } I=0, U=1, MS[I]=0xdad5b294ccd9aac9 
+003 - MS[] = { a5 9b b5 69 25 } I=1, U=1, MS[I]=0xd2d54db59932a59b 
+007 - MS[] = { 93 25 99 d3 93 } I=2, U=2, MS[I]=0x5966a9565536a699 
+012 - MS[] = { 93 25 95 6b c9 } I=3, U=0, MS[I]=0xdad4d2db3592a96b 
+017 - MS[] = { 59 a9 29 2d 49 } I=4, U=4, MS[I]=0x2b5694b329ab6a49 
+018 - MS[] = { 59 a9 29 2d 99 } I=0, U=0, MS[I]=0xb324a994952d2959 
+019 - MS[] = { 4b a9 29 2d 99 } I=1, U=1, MS[I]=0x4992964a5269b4a9 
+022 - MS[] = { 4b a5 2d 4b 99 } I=2, U=4, MS[I]=0x6cac9b36aad3652d 
+026 - MS[] = { 35 a9 d3 4b 9b } I=3, U=3, MS[I]=0xaa996b2d9b29934b 
+032 - MS[] = { d5 4d 2b 65 93 } I=4, U=4, MS[I]=0x369b566ad4b49a93 
+037 - MS[] = { 69 35 a5 2d 9b } I=0, U=0, MS[I]=0xb2cdaa56d6564a69 
+043 - MS[] = { ad a5 35 a9 ad } I=1, U=4, MS[I]=0xa559966ada49aca5 
+045 - MS[] = { ad a5 35 a9 6d } I=2, U=0, MS[I]=0x2a9a99b66d649935
+$ ./a.out
+004 - MS[] = { 53 95 cb 5b cd } I=0, U=0, MS[I]=0xaacca94a952cc953 
+005 - MS[] = { cd 95 cb 5b cd } I=1, U=1, MS[I]=0xa5332b2b2db6a995 
+008 - MS[] = { cd ab 65 33 cd } I=2, U=4, MS[I]=0x266d24a56d2cc965 
+010 - MS[] = { b5 ab 65 33 5b } I=3, U=1, MS[I]=0xd65956b266da6933 
+011 - MS[] = { b5 a9 65 33 5b } I=4, U=2, MS[I]=0x94b6da9996d2925b 
+012 - MS[] = { b5 a9 65 33 5b } I=0, U=3, MS[I]=0xadb4a692acaaa4b5 
+016 - MS[] = { c9 33 d9 69 69 } I=1, U=0, MS[I]=0x9a99b6d4a6daa533 
+019 - MS[] = { 4d 2d a5 b5 9b } I=2, U=3, MS[I]=0xd6a6554b4d52a4a5 
+024 - MS[] = { b3 93 db 33 db } I=3, U=3, MS[I]=0x2ab65256926dab33 
+
